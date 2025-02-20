8251105490e81f46e374e0bee0efd1921b87f9d0
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2025-02-20 09:38:20 +0100

    Initial commit

---
+++ src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
@@ -0,0 +1,56 @@
+package com.montaury.pokebagarre.metier;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PokemonTest {
+    private String POKEMON1_WIN = "Le premier pokemon a gagné";
+    private String POKEMON2_WIN = "Le deuxième pokemon a gagné";
+
+    @Test
+    void different_attack_pokemon1_better_attack_win() {
+        // Given
+        Pokemon pokemon1 = new Pokemon("Premier pokemon", null, new Stats(150, 150));
+        Pokemon pokemon2 = new Pokemon("Deuxième pokemon", null, new Stats(100, 100));
+
+        // When
+        boolean resultat = pokemon1.estVainqueurContre(pokemon2);
+
+        // Then
+        assertThat(resultat).isEqualTo(true);
+    }
+
+    @Test
+    void different_attack_pokemon2_better_attack_win() {
+        Pokemon pokemon1 = new Pokemon("Premier pokemon", null, new Stats(100, 100));
+        Pokemon pokemon2 = new Pokemon("Deuxième pokemon", null, new Stats(150, 150));
+        final boolean resultat = pokemon1.estVainqueurContre(pokemon2);
+        assertFalse(resultat, POKEMON2_WIN);
+    }
+
+    @Test
+    void same_attack_different_defense_pokemon1_better_defense_win() {
+        Pokemon pokemon1 = new Pokemon("Premier pokemon", null, new Stats(150, 150));
+        Pokemon pokemon2 = new Pokemon("Deuxième pokemon", null, new Stats(150, 100));
+        final boolean resultat = pokemon1.estVainqueurContre(pokemon2);
+        assertTrue(resultat, POKEMON1_WIN);
+    }
+
+    @Test
+    void same_attack_different_defense_pokemon2_better_defense_win() {
+        Pokemon pokemon1 = new Pokemon("Premier pokemon", null, new Stats(150, 150));
+        Pokemon pokemon2 = new Pokemon("Deuxième pokemon", null, new Stats(150, 200));
+        final boolean resultat = pokemon1.estVainqueurContre(pokemon2);
+        assertFalse(resultat, POKEMON2_WIN);
+    }
+
+    @Test
+    void same_attack_same_defense_pokemon1_win() {
+        Pokemon pokemon1 = new Pokemon("Premier pokemon", null, new Stats(150, 150));
+        Pokemon pokemon2 = new Pokemon("Deuxième pokemon", null, new Stats(150, 150));
+        final boolean resultat = pokemon1.estVainqueurContre(pokemon2);
+        assertTrue(resultat, POKEMON2_WIN);
+    }
+}
